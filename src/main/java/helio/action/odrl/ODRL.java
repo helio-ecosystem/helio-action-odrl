80d0b6a89a02ce05a77f6886008ed5765c9851aa
Andrea Cimmino Arriaga <cimmino@fi.upm.es>
2023-04-25 17:27:03 +0200

    integrated new version of odrl

--- src/main/java/helio/action/odrl/ODRL.java
+++ src/main/java/helio/action/odrl/ODRL.java
@@ -1,5 +1,6 @@
 package helio.action.odrl;
 
+import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Map;
 
@@ -10,53 +11,60 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
-import java.lang.reflect.Type;
 
 import helio.blueprints.Action;
 import helio.blueprints.exceptions.ActionException;
-import odrl.lib.OdrlLib;
-import odrl.lib.exceptions.OdrlRegistrationException;
-import odrl.lib.operands.Time;
+import odrl.lib.model.OdrlLib;
+import odrl.lib.model.exceptions.OdrlRegistrationException;
+import odrl.lib.model.functions.Time;
 
 public class ODRL implements Action{
 
 	private Logger logger = LoggerFactory.getLogger(ODRL.class);
 
 	private OdrlLib odrl = new OdrlLib();
-	
+
 	protected static final Gson GSON = new Gson();
 	protected static final Type empMapType = new TypeToken<Map<String, String>>() {}.getType();
-	
+
 	public ODRL() {
 		odrl.registerPrefix("ops", "http://upm.es/operands#");
 		try {
+			odrl.registerGeof();
+			odrl.registerSpatial();
+			odrl.registerNative();
 			odrl.register("ops", new Time());
 		} catch (OdrlRegistrationException e) {
 			logger.error(e.getMessage());
 			throw new IllegalArgumentException(e.getMessage());
 		}
-		
+
 	}
-	
+
+	@Override
 	public void configure(JsonObject configuration) {
-		if(configuration.has("prefixes")) {
-			JsonObject prefixesJson = configuration.get("prefixes").getAsJsonObject(); 
+		if(configuration!=null && configuration.has("prefixes")) {
+			JsonObject prefixesJson = configuration.get("prefixes").getAsJsonObject();
 			Map<String, String>prefixes = GSON.fromJson(prefixesJson, empMapType);
 			prefixes.entrySet().parallelStream().forEach(entry -> odrl.registerPrefix(entry.getKey(), entry.getValue()));
 
 		}
 	}
 
+	@Override
 	public String run(String values) throws ActionException {
 		JsonObject result = new JsonObject();
 		try {
-			JsonObject policy = GSON.fromJson(values, JsonObject.class);
-			Map<String, List<String>> accessRights = odrl.solve(policy);
-			if(!accessRights.isEmpty())
-				accessRights.entrySet().forEach(entry -> result.add(entry.getKey(), toJsonArray(entry.getValue())));
+			if(values!=null && !values.isBlank()) {
+				JsonObject policy = GSON.fromJson(values, JsonObject.class);
+				Map<String, List<String>> accessRights = odrl.solve(policy);
+				if(!accessRights.isEmpty())
+					accessRights.entrySet().forEach(entry -> result.add(entry.getKey(), toJsonArray(entry.getValue())));
+			}
 			}catch(Exception e) {
 				throw new ActionException(e.getMessage());
 			}
+
 		return result.toString();
 	}
 
--- src/main/java/helio/action/odrl/Test.java
+++ src/main/java/helio/action/odrl/Test.java
@@ -1,5 +1,7 @@
 package helio.action.odrl;
 
+import com.google.gson.JsonObject;
+
 import helio.blueprints.exceptions.ActionException;
 
 public class Test {
@@ -26,9 +28,31 @@ public class Test {
 			+ "    }\n"
 			+ "  ]\n"
 			+ "}";
-	
+
+	public static String data2 ="{\n"
+			+ " \"@context\": [\"http://www.w3.org/ns/odrl.jsonld\",\n"
+			+ "   {\"geof\" : \"http://www.opengis.net/def/function/geosparql/\"}\n"
+			+ " ],\n"
+			+ " \"@type\": \"Set\",\n"
+			+ " \"uid\": \"http://example.com/policy:1010\",\n"
+			+ " \"permission\": [{\n"
+			+ " 	\"target\": \"http://example.com/asset:9898.movie\",\n"
+			+ "	\"action\": \"read\",\n"
+			+ "	\"constraint\":{\n"
+			+ "          \"leftOperand\":  { \"@value\": \"POLYGON((-3.840070031583308 40.405675773960866,-3.839853107929229 40.40574011056009,-3.839842379093169 40.40571994155346,-3.839847072958945 40.40571840972993,-3.839840702712535 40.405706155140365,-3.840056620538234 40.40564947763477,-3.840070031583308 40.405675773960866))\", \"@type\": \"http://www.opengis.net/ont/geosparql#wktLiteral\" },\n"
+			+ "          \"operator\": \"geof:sfContains\" ,\n"
+			+ "          \"rightOperand\": { \"@value\": \"POINT((-3.839907742614103 40.40570902334372))\", \"@type\": \"http://www.opengis.net/ont/geosparql#wktLiteral\" }\n"
+			+ "\n"
+			+ "	   }\n"
+			+ " }]\n"
+			+ "}\n"
+			+ "";
+
 	public static void main(String[] args) throws ActionException {
 		ODRL odrl = new ODRL();
-		System.out.println(odrl.run(data));
+		JsonObject conf = new JsonObject();
+		conf.addProperty("geof", "http://www.opengis.net/def/function/geosparql/");
+		odrl.configure(conf);
+		System.out.println(odrl.run(data2));
 	}
 }
